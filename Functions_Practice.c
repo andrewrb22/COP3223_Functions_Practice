f9660ada7a208ff65c5476486f1317eb412d4ee9
Andrew Rodriguez <andrewrb22@gmail.com>
2022-03-16 19:37:46 -0400

    Created a menu function, added toupper function

--- Functions_Practice.c
+++ Functions_Practice.c
@@ -3,27 +3,66 @@
 
 void getNumbers (int *, int *); // Prototype of the function
 void displaynumbers(int, int); //Prototype of the function
+char menu();
+
+//--------------------------------------------
 int main(){
     
     int x = 0, y = 0;
     
-    getNumbers(&x,&y);
+    //getNumbers(&x,&y);
 
-   displaynumbers(x,y);
+   //displaynumbers(x,y);
+
+   char selection;
+
+   selection = menu();
+
+   printf("you have selected %c", selection);
 
     return 0;
 
 }// End of Main
+
 //---------------------------------------------------
+
 void getNumbers(int *a, int *b){
 
 printf("\nEnter your two numbers: ");
 scanf("%d%d", a, b);
 
 }//End of getNumbers
+
 //-------------------------------------------------
+
 void displaynumbers(int number1, int number2){
 
 printf("\nYou entered %d and %d", number1, number2);
 
 }//End of displaynumbers
+
+//-----------------------------
+
+char menu(){
+    
+    char option;
+do {
+    printf("\nWelcome to my program\n");
+    printf("Choose from the following menu:\n");
+
+    printf("a/A : to print the sum of the numbers\n");
+    printf("p/P : to print the product of the numbers\n");
+    printf("d/D : to print the divsion of the first numbers by the second number\n");
+
+    printf("Enter your selection: ");
+    scanf("%c", &option);
+    option = toupper(option);
+
+}while (option != 'A' && option != 'P' && option != 'D' );
+
+return option;
+
+
+}//End of menu
+
+//----------------------------------------------------------
