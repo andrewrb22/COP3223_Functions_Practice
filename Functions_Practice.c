041d93c5e49d83890141b9f9661b1d39940c7ce7
Andrew Rodriguez <andrewrb22@gmail.com>
2022-03-16 19:03:33 -0400

    functions estructure and example

--- Functions_Practice.c
+++ Functions_Practice.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void getNumbers (int a, int b); // Prototype of the function
+
 int main(){
     
-    printf("\n\tLet's Begin with the Functions Practice\n");
+    int x = 0, y = 0;
+    
+    getNumbers(x,y);
 
+    printf("\nYou Entered %d and %d", x ,y);
+    
     return 0;
-}
+
+}// End of Main
+
+void getNumbers(int a, int b){
+
+printf("\nEnter your two numbers: ");
+scanf("%d%d", &a, &b);
+
+}//End of getNumbers
