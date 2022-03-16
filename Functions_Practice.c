9b672f186a839eea8f2acf8d997552c244be62fb
Andrew Rodriguez <andrewrb22@gmail.com>
2022-03-16 19:18:42 -0400

    created new function to display numbers entered

--- Functions_Practice.c
+++ Functions_Practice.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 
 void getNumbers (int *, int *); // Prototype of the function
-
+void displaynumbers(int, int); //Prototype of the function
 int main(){
     
     int x = 0, y = 0;
     
     getNumbers(&x,&y);
 
-    printf("\nYou Entered %d and %d", x ,y);
+   displaynumbers(x,y);
 
     return 0;
 
 }// End of Main
-
+//---------------------------------------------------
 void getNumbers(int *a, int *b){
 
 printf("\nEnter your two numbers: ");
 scanf("%d%d", a, b);
 
 }//End of getNumbers
+//-------------------------------------------------
+void displaynumbers(int number1, int number2){
+
+printf("\nYou entered %d and %d", number1, number2);
+
+}//End of displaynumbers
